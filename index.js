41cbe65960d7adec3794ab2ba85ab2dc50f9f5ef
ayiis <76609039+ayiis218@users.noreply.github.com>
2022-06-18 19:43:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,90 +1,90 @@
-var http = require('http');
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var port = 8080;
+var http = require('http')
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+var port = 8080
 
-var mysql = require('mysql');
+var mysql = require('mysql')
 var db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     database:"pijarcamp"
-});
+})
+
 db.connect((err) => {
-    if (err) throw err;
-    console.log("Connected!");
-});
+    if (err)throw err
+    console.log("Connected!")
+})
 
 app.use(bodyParser.json());
 var server =  app.listen(port, () => {
-  console.info('listening on %d', port);
+  console.info('listening on %d', port)
 })
 
 app.use(bodyParser.urlencoded({
   extended: true
 }))
 
-app.engine('html', require('ejs').renderFile);
-app.use(express.static('public'));
+app.engine('html', require('ejs').renderFile)
+app.use(express.static('public'))
 
 app.get('/', function (req, res) {
-	let s = "SELECT * FROM produk";
+	let s = "SELECT * FROM produk"
 	let query = db.query(s, (err, results)=>{
 		var data = {
 			data:results
 		}
-		res.render(__dirname+"/public/read.html", data);
-	});
-});
+		res.render(__dirname+"/public/read.html", data)
+	})
+})
 
 app.get('/insert', function (req, res) {
-	res.render(__dirname+"/public/insert.html");
-});
+	res.render(__dirname+"/public/insert.html")
+})
 
 app.get('/update', function (req, res) {
-	let s = "SELECT * FROM produk WHERE nama_produk='"+req.query.i+"'";
+	let s = "SELECT * FROM produk WHERE nama_produk='"+req.query.i+"'"
 	let query = db.query(s, (err, results)=>{
 		var data = {
 			data:results[0]
 		}
-		res.render(__dirname+"/public/update.html", data);
-	});
-
-});
+		res.render(__dirname+"/public/update.html", data)
+	})
+})
 
 app.post("/tambah", function (req, res) {
     let data = {nama_produk: req.body.nama_produk, keterangan: req.body.keterangan, harga:req.body.harga, jumlah:req.body.jumlah};
-    let sql = "INSERT INTO produk SET ?";
+    let sql = "INSERT INTO produk SET ?"
     let query = db.query(sql, data,(err, results) => {
        if (err) {
-       		res.send({result:"error"});
+       		res.send({result:"error"})
        }else{
-       		res.redirect("/");
+       		res.redirect("/")
        }
     });
 });
 
 app.get("/hapus", function (req, res) {
-	 let sql = "DELETE FROM produk WHERE nama_produk='"+req.query.i+"'";
+	 let sql = "DELETE FROM produk WHERE nama_produk='"+req.query.i+"'"
     let query = db.query(sql, (err, results) => {
        if (err) {
-       		res.send({result:"error"});
+       		res.send({result:"error"})
        }else{
-       		res.redirect("/");
+       		res.redirect("/")
        }
-    });
-});
+    })
+})
 
 app.post("/update", function (req, res) {
 	let data = {nama_produk: req.body.nama_produk, keterangan: req.body.keterangan, harga:req.body.harga, jumlah:req.body.jumlah};
-    let sql = "UPDATE produk SET ? WHERE nama_produk='"+req.query.i+"'";
+    let sql = "UPDATE produk SET ? WHERE nama_produk='"+req.query.i+"'"
     let query = db.query(sql, data,(err, results) => {
        if (err) {
-       		console.log("error");
-       		res.send({result:"error"});
+       		console.log("error")
+       		res.send({result:"error"})
        }else{
-       		console.log("success");
-       		res.redirect("/");
+       		console.log("success")
+       		res.redirect("/")
        }
-    });
+    })
 })
